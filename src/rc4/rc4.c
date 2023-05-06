2c183e4ef0b37c2026c60964dfc2bfd8503849ed
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-05-06 17:36:51 +0200

    Push Update 06/05/2023, 17:36:51

--- src/rc4/rc4.c
+++ src/rc4/rc4.c
@@ -5,19 +5,20 @@ void rc4_encode_decode(const unsigned char* data,unsigned int data_length,const 
 	}
 	unsigned char j=0;
 	for (unsigned int i=0;i<246;i++){
-		j+=S[i]+key[i%key_length];
-		unsigned char tmp=S[i];
+		unsigned char k=S[i];
+		j+=k+key[i%key_length];
 		S[i]=S[j];
-		S[j]=tmp;
+		S[j]=k;
 	}
 	unsigned char i=0;
 	j=0;
 	for (unsigned int k=0;k<data_length;k++){
 		i++;
-		j+=S[i];
-		unsigned char tmp=S[i];
-		S[i]=S[j];
-		S[j]=tmp;
-		out[k]=data[k]^S[(S[i]+S[j])&255];
+		unsigned char l=S[i];
+		j+=l;
+		unsigned char m=S[j];
+		S[i]=m;
+		S[j]=l;
+		out[k]=data[k]^S[(l+m)&255];
 	}
 }
